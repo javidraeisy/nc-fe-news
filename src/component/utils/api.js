273994a6a8dd4b34efdff5474032aa4f6c351b8a
Your Name <youremail@domain.com>
2023-04-18 12:09:45 +0100

    finished task5

--- src/component/utils/api.js
+++ src/component/utils/api.js
@@ -10,3 +10,9 @@ export const getArticles = () => {
     return data.articles;
   });
 };
+
+export const getArticleById = (article_id) => {
+  return newsApi.get(`/articles/${article_id}`).then(({ data }) => {
+    return data.article;
+  });
+};
