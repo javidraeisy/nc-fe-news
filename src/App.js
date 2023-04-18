20c4bea3336e98e4dbc6a3a4c7287aaab3837a3f
Your Name <youremail@domain.com>
2023-04-18 10:16:08 +0100

    finished task4

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import { Route,Routes } from 'react-router-dom';
-import './App.css';
-import Articles from './component/Articles';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Articles from "./component/Articles";
+import Header from "./component/Header/Header";
+
 function App() {
   return (
     <div className="App">
+    <Header />
       <Routes>
         <Route path="/" element={<Articles />} />
         <Route path="/articles" element={<Articles />} />
@@ -13,3 +16,4 @@ function App() {
 }
 
 export default App;
+
--- src/component/ArticleCard.jsx
+++ src/component/ArticleCard.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
+
+
+
 export const ArticleCard = ({
   article_img_url,
   title,
